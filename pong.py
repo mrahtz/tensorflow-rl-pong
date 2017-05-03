45ed25f9bc3a204e842055f25e4f672b028bc2f8
Matthew Rahtz <matthew.rahtz@gmail.com>
2017-05-03 08:09:18 +0200

    Use Xavier initialiser and a batch size of 10 episodes

--- pong.py
+++ pong.py
@@ -10,6 +10,8 @@ from IPython.core.debugger import Tracer
 
 OBSERVATION_SIZE = 6400
 HIDDEN_LAYER_SIZE = 200
+BATCH_SIZE_EPISODES = 2
+RENDER = False
 
 UP_ACTION = 2
 DOWN_ACTION = 3
@@ -19,10 +21,12 @@ sess = tf.InteractiveSession()
 x = tf.placeholder(tf.float32, [None, OBSERVATION_SIZE], name='x')
 y = tf.placeholder(tf.float32, [None], name='y')
 
-x1 = tf.layers.dense(x, units=HIDDEN_LAYER_SIZE)
+x1 = tf.layers.dense(x, units=HIDDEN_LAYER_SIZE,
+        kernel_initializer=tf.contrib.layers.xavier_initializer())
 x1 = tf.nn.relu(x1)
 
-logp = tf.layers.dense(x1, units=1)
+logp = tf.layers.dense(x1, units=1,
+        kernel_initializer=tf.contrib.layers.xavier_initializer())
 p_op = tf.sigmoid(logp)
 
 loss_op = tf.reduce_mean(y - p_op)
@@ -44,66 +48,75 @@ def prepro(I):
   return I.astype(np.float).ravel()
 
 
-def process_reward(reward, state_action_pairs):
-    states, actions = zip(*state_action_pairs)
+def train(state_action_reward_tuples):
+    print("Training based on %d (state, action, reward) tuples" %
+            len(state_action_reward_tuples))
+
+    states, actions, rewards = zip(*state_action_reward_tuples)
     states = np.array(states)
     actions = np.array(actions)
+    rewards = np.array(rewards)
 
-    if reward == +1:
-        desired_actions = actions
-    elif reward == -1:
-        desired_actions = np.logical_not(actions).astype(np.int32)
+    actions[rewards == -1] = \
+            np.logical_not(actions[rewards == -1]).astype(np.int)
+    sess.run(train_op, feed_dict={x: states, y: actions})
 
-    sess.run(train_op, feed_dict={x: states, y: desired_actions})
 
-action = env.action_space.sample()
-observation = env.reset()
 last_observation = None
-t = 0
-game_n = 0
-state_action_pairs = []
+episode_n = 1
 
 while True:
-    #env.render()
-    observation, reward, done, info = env.step(action)
-    t += 1
+    print("Episode %d" % episode_n)
+    env.reset()
+    action = env.action_space.sample()
+    state_action_tuples = []
+    state_action_reward_tuples = []
+    done = False
 
-    if reward != 0:
-        print("Game %d: %d steps; " % (game_n, t), end='')
-        if reward == +1:
-            print("won!")
+    game_n = 1
+    t = 0
+    while not done:
+        if RENDER:
+            env.render()
+
+        observation, reward, done, info = env.step(action)
+        observation = prepro(observation)
+        t += 1
+
+        if reward == -1:
+            print("Game %d: %d time steps; lost..." % (game_n, t))
+        elif reward == +1:
+            print("Game %d: %d time steps; won!" % (game_n, t))
+        if reward != 0:
+            game_n += 1
+            t = 0
+
+            states, actions = zip(*state_action_tuples)
+            assert(len(states) == len(actions))
+            rewards = len(states) * [reward]
+            assert(len(states) == len(rewards))
+            state_action_reward_tuples.extend(zip(states, actions, rewards))
+            state_action_tuples = []
+
+        if last_observation is not None:
+            observation_delta = observation - last_observation
+            last_observation = observation
         else:
-            print("lost!")
+            last_observation = observation
+            continue
 
-        if t > 100:
-            reward = +1
+        up_probability = \
+            sess.run(p_op, feed_dict={x: observation_delta.reshape([1, -1])})
+        up_probability = up_probability[0]
 
-        if reward == +1:
-            process_reward(reward, state_action_pairs)
+        if np.random.uniform() < up_probability:
+            action = UP_ACTION
+        else:
+            action = DOWN_ACTION
 
-        state_action_pairs = []
-        t = 0
-        game_n += 1
-        observation = env.reset()
-        last_observation = None
-        action = env.action_space.sample()
+        state_action_tuples.append((observation_delta, action_dict[action]))
 
-    observation = prepro(observation)
-
-    if last_observation is not None:
-        observation_delta = observation - last_observation
-        last_observation = observation
-    else:
-        last_observation = observation
-        continue
-
-    up_probability = \
-        sess.run(p_op, feed_dict={x: observation_delta.reshape([1, -1])})
-    up_probability = up_probability[0]
-
-    if np.random.uniform() < up_probability:
-        action = UP_ACTION
-    else:
-        action = DOWN_ACTION
-
-    state_action_pairs.append((observation_delta, action_dict[action]))
+    print("Episode %d finished after %d games" % (episode_n, game_n))
+    if episode_n % BATCH_SIZE_EPISODES == 0:
+        train(state_action_reward_tuples)
+    episode_n += 1
