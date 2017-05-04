38b57db2c81ab69cae8cbd670765650e72fe2b09
Matthew Rahtz <matthew.rahtz@gmail.com>
2017-05-04 08:28:16 +0200

    Clean up code

--- pong.py
+++ pong.py
@@ -2,46 +2,29 @@
 
 from __future__ import print_function
 
+import argparse
 import numpy as np
 import tensorflow as tf
 import gym
 
-from IPython.core.debugger import Tracer
+parser = argparse.ArgumentParser()
+parser.add_argument('--hidden_layer_size', type=int, default=200)
+parser.add_argument('--batch_size_episodes', type=int, default=10)
+parser.add_argument('--render', action='store_true')
+args = parser.parse_args()
 
 OBSERVATION_SIZE = 6400
-HIDDEN_LAYER_SIZE = 200
-BATCH_SIZE_EPISODES = 10
-RENDER = False
-
+# Action values to send to environment to move paddle up/down
 UP_ACTION = 2
 DOWN_ACTION = 3
+# Mapping from action values to outputs from the policy network
 action_dict = {DOWN_ACTION: 0, UP_ACTION: 1}
 
-sess = tf.InteractiveSession()
-x = tf.placeholder(tf.float32, [None, OBSERVATION_SIZE], name='x')
-y = tf.placeholder(tf.float32, [None], name='y')
-advantage = tf.placeholder(tf.float32, [None], name='advantage')
-
-x1 = tf.layers.dense(x, units=HIDDEN_LAYER_SIZE,
-        kernel_initializer=tf.contrib.layers.xavier_initializer())
-x1 = tf.nn.relu(x1)
-
-logp = tf.layers.dense(x1, units=1,
-        kernel_initializer=tf.contrib.layers.xavier_initializer())
-p_op = tf.sigmoid(logp)
-
-batch_losses = -1 * advantage * (y * tf.log(p_op) + (1 - y) * tf.log(1 - p_op))
-loss_op = tf.reduce_mean(batch_losses)
-optimizer = tf.train.RMSPropOptimizer(learning_rate=1e-3)
-train_op = optimizer.minimize(loss_op)
-
-tf.global_variables_initializer().run()
-
-env = gym.make('Pong-v0')
 
 def prepro(I):
   """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
-  """https://gist.github.com/karpathy/a4166c7fe253700972fcbc77e4ea32c5"""
+  # From Karpathy's code:
+  # https://gist.github.com/karpathy/a4166c7fe253700972fcbc77e4ea32c5
   I = I[35:195] # crop
   I = I[::2,::2,0] # downsample by factor of 2
   I[I == 144] = 0 # erase background (background type 1)
@@ -51,7 +34,7 @@ def prepro(I):
 
 
 def train(state_action_reward_tuples):
-    print("Training based on %d (state, action, reward) tuples" %
+    print("Training with %d (state, action, reward) tuples" %
             len(state_action_reward_tuples))
 
     states, actions, rewards = zip(*state_action_reward_tuples)
@@ -59,64 +42,112 @@ def train(state_action_reward_tuples):
     actions = np.array(actions)
     rewards = np.array(rewards)
 
-    sess.run(train_op, feed_dict={x: states, y: actions, advantage: rewards})
+    sess.run(train_op, feed_dict={observations: states,
+                                  sampled_actions: actions,
+                                  advantage: rewards})
 
 
-last_observation = None
+# TensorFlow setup
+
+sess = tf.InteractiveSession()
+observations = tf.placeholder(tf.float32, [None, OBSERVATION_SIZE])
+# +1 for up, -1 for down
+sampled_actions = tf.placeholder(tf.float32, [None])
+advantage = tf.placeholder(tf.float32, [None], name='advantage')
+
+x = tf.layers.dense(observations, units=args.hidden_layer_size,
+        kernel_initializer=tf.contrib.layers.xavier_initializer())
+x = tf.nn.relu(x)
+
+x = tf.layers.dense(x, units=1,
+        kernel_initializer=tf.contrib.layers.xavier_initializer())
+up_probability = tf.sigmoid(x)
+
+# Train based on the log probability of the sampled action.
+#
+# If the sampled action was part of a round in which the agent won, we assume
+# the sampled action was a good one, so we try to maximise the log probability
+# of that action in the future. Here, therefore, 'advantage' is positive.
+#
+# If the sampled action was part of a round in which the agent didn't win, we
+# assume the sampled action was a bad choice, so in that case we want to
+# _minimise_ the log probability of that action in the future. 'advantage' is
+# therefore negative.
+#
+# (The '-1' at the end is necessary because with TensorFlow we can only minimise
+# a loss, whereas we actually want to maximise this quantity. For e.g. a good
+# sampled action, we want to maximise the log probability, and this is the same
+# as minimising the negative log probability.)
+batch_losses = \
+        (
+            sampled_actions       * tf.log(up_probability) + \
+            (1 - sampled_actions) * tf.log(1 - up_probability) \
+        ) \
+        * advantage \
+        * -1
+loss = tf.reduce_mean(batch_losses)
+optimizer = tf.train.RMSPropOptimizer(learning_rate=1e-3)
+train_op = optimizer.minimize(loss)
+
+tf.global_variables_initializer().run()
+
+# Set up OpenAI gym environment
+
+env = gym.make('Pong-v0')
+
+# Start training!
+
 episode_n = 1
 
 while True:
-    print("Episode %d" % episode_n)
-    env.reset()
-    action = env.action_space.sample()
+    print("Starting episode %d" % episode_n)
+
     state_action_tuples = []
     state_action_reward_tuples = []
-    done = False
+    episode_done = False
 
-    game_n = 1
-    t = 0
-    while not done:
-        if RENDER:
+    last_observation = env.reset()
+    last_observation = prepro(last_observation)
+    action = env.action_space.sample()
+    observation, _, _, _ = env.step(action)
+    observation = prepro(observation)
+    n_steps = 1
+
+    round_n = 1
+
+    while not episode_done:
+        if args.render:
             env.render()
 
-        observation, reward, done, info = env.step(action)
-        observation = prepro(observation)
-        t += 1
-
-        if reward == -1:
-            print("Game %d: %d time steps; lost..." % (game_n, t))
-        elif reward == +1:
-            print("Game %d: %d time steps; won!" % (game_n, t))
-        if reward != 0:
-            game_n += 1
-            t = 0
-
-            states, actions = zip(*state_action_tuples)
-            assert(len(states) == len(actions))
-            rewards = len(states) * [reward]
-            assert(len(states) == len(rewards))
-            state_action_reward_tuples.extend(zip(states, actions, rewards))
-            state_action_tuples = []
-
-        if last_observation is not None:
-            observation_delta = observation - last_observation
-            last_observation = observation
-        else:
-            last_observation = observation
-            continue
-
-        up_probability = \
-            sess.run(p_op, feed_dict={x: observation_delta.reshape([1, -1])})
-        up_probability = up_probability[0]
-
-        if np.random.uniform() < up_probability:
+        observation_delta = observation - last_observation
+        last_observation = observation
+        up_probability_val = sess.run(up_probability,
+            feed_dict={observations: observation_delta.reshape([1, -1])})
+        up_probability_val = up_probability_val[0]
+        if np.random.uniform() < up_probability_val:
             action = UP_ACTION
         else:
             action = DOWN_ACTION
-
         state_action_tuples.append((observation_delta, action_dict[action]))
 
-    print("Episode %d finished after %d games" % (episode_n, game_n))
-    if episode_n % BATCH_SIZE_EPISODES == 0:
+        observation, reward, episode_done, info = env.step(action)
+        observation = prepro(observation)
+        n_steps += 1
+
+        if reward == -1:
+            print("Round %d: %d time steps; lost..." % (round_n, n_steps))
+        elif reward == +1:
+            print("Round %d: %d time steps; won!" % (round_n, n_steps))
+        if reward != 0:
+            round_n += 1
+            n_steps = 0
+
+            states, actions = zip(*state_action_tuples)
+            rewards = len(states) * [reward]
+            state_action_reward_tuples.extend(zip(states, actions, rewards))
+            state_action_tuples = []
+
+    print("Episode %d finished after %d rounds" % (episode_n, round_n))
+    if episode_n % args.batch_size_episodes == 0:
         train(state_action_reward_tuples)
     episode_n += 1
