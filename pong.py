15c5a9c14fa9b8512429de942eb31a55f433d1eb
Matthew Rahtz <matthew.rahtz@gmail.com>
2017-05-07 05:54:54 +0000

    Increase checkpoint frequency

--- pong.py
+++ pong.py
@@ -13,7 +13,7 @@ from model import Network
 parser = argparse.ArgumentParser()
 parser.add_argument('--hidden_layer_size', type=int, default=200)
 parser.add_argument('--batch_size_episodes', type=int, default=1)
-parser.add_argument('--checkpoint_every_n_episodes', type=int, default=1)
+parser.add_argument('--checkpoint_every_n_episodes', type=int, default=10)
 parser.add_argument('--discount_factor', type=int, default=0.99)
 parser.add_argument('--render', action='store_true')
 parser.add_argument('run_id', type=str)
