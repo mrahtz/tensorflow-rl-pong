619a9440fd703a15daafdba93bc24d92888facac
Matthew Rahtz <matthew.rahtz@gmail.com>
2017-05-08 05:00:27 +0200

    yapf

--- policy_network.py
+++ policy_network.py
@@ -3,23 +3,28 @@ import numpy as np
 
 OBSERVATIONS_SIZE = 6400
 
+
 class Network:
-
-
     def __init__(self, hidden_layer_size):
         self.sess = tf.InteractiveSession()
 
-        self.observations = tf.placeholder(tf.float32, [None, OBSERVATIONS_SIZE])
+        self.observations = tf.placeholder(tf.float32,
+                                           [None, OBSERVATIONS_SIZE])
         # +1 for up, -1 for down
         self.sampled_actions = tf.placeholder(tf.float32, [None, 1])
-        self.advantage = tf.placeholder(tf.float32, [None, 1], name='advantage')
+        self.advantage = tf.placeholder(
+            tf.float32, [None, 1], name='advantage')
 
-        x = tf.layers.dense(self.observations, units=hidden_layer_size,
+        x = tf.layers.dense(
+            self.observations,
+            units=hidden_layer_size,
             use_bias=False,
             kernel_initializer=tf.contrib.layers.xavier_initializer())
         x = tf.nn.relu(x)
 
-        x = tf.layers.dense(x, units=1,
+        x = tf.layers.dense(
+            x,
+            units=1,
             use_bias=False,
             kernel_initializer=tf.contrib.layers.xavier_initializer())
         self.up_probability = tf.sigmoid(x)
@@ -39,15 +44,16 @@ class Network:
         # a loss, whereas we actually want to maximise this quantity. For e.g. a good
         # sampled action, we want to maximise the log probability, and this is the same
         # as minimising the negative log probability.)
-        self.loss = tf.losses.log_loss(labels=self.sampled_actions, predictions=self.up_probability,
-                weights=self.advantage)
-        optimizer = tf.train.AdamOptimizer(learning_rate=0.001/2)
+        self.loss = tf.losses.log_loss(
+            labels=self.sampled_actions,
+            predictions=self.up_probability,
+            weights=self.advantage)
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001 / 2)
         self.train_op = optimizer.minimize(self.loss)
 
         tf.global_variables_initializer().run()
-        
+
         self.saver = tf.train.Saver()
-        
 
     def load_checkpoint(self, checkpoints_dir):
         print("Loading checkpoint...")
@@ -57,21 +63,24 @@ class Network:
         self.saver.save(self.sess, 'checkpoints/model.ckpt')
 
     def forward_pass(self, observations):
-        up_probability_val = self.sess.run(self.up_probability,
+        up_probability = self.sess.run(
+            self.up_probability,
             feed_dict={self.observations: observations.reshape([1, -1])})
-        up_probability_val = up_probability_val[0]
+        up_probability = up_probability[0]
         return up_probability_val
-
 
     def train(self, state_action_reward_tuples):
         print("Training with %d (state, action, reward) tuples" %
-                len(state_action_reward_tuples))
+              len(state_action_reward_tuples))
 
         states, actions, rewards = zip(*state_action_reward_tuples)
         states = np.vstack(states)
         actions = np.vstack(actions)
         rewards = np.vstack(rewards)
 
-        self.sess.run(self.train_op, feed_dict={self.observations: states,
-                                      self.sampled_actions: actions,
-                                      self.advantage: rewards})
+        feed_dict = {
+            self.observations: states,
+            self.sampled_actions: actions,
+            self.advantage: rewards
+        }
+        self.sess.run(self.train_op, feed_dict)
